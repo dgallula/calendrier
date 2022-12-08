ff5c230d89d9e9fa7a30fcb1592fa036d8d737a1
david <dgallula@gmail.com>
2022-12-08 15:33:10 +0200

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+try{
+    $DB = new PDO('mysql:host=localhost;dbname=calendrier','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
+}
+catch(PDOException $e){
+    echo 'Base de donnée en vacance';
+    exit();
+}
+?>
+
